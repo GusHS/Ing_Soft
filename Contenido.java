6fe24be5cb66b59f93bd4845a570ee8b19d0ead6
Gustavo Hernández <GusHS@MacBook-Air-de-Gustavo.local>
2018-11-17 09:28:34 -0600

    finished content class

---
+++ Contenido.java
@@ -0,0 +1,66 @@
+import javax.swing.ImageIcon;
+
+public class Contenido {
+    private int idLectura, nDescargas;
+    private String estado, texto;
+    private ImageIcon imagen;
+    private Grupo grupo;
+
+    public Contenido(){
+        this.idLectura= 0;
+        this.estado ="";
+        this.texto="";
+        this.imagen = new ImageIcon();
+        this.grupo = new Grupo();
+        this.nDescargas = 0;
+    }
+
+    public Contenido(String datos, ImageIcon img, Grupo group){
+        String[]sp = datos.split("_");
+        this.idLectura = Integer.parseInt(sp[0]);
+        this.estado = sp[1];
+        this.texto=sp[2];
+        this.nDescargas = Integer.parseInt(sp[3]);
+        this.imagen = img;
+        this.grupo = group;
+    }
+
+    public int getIdLectura(){
+        return this.idLectura;
+    }
+    public String getEstado(){
+        return this.estado;
+    }
+    public String getTexto(){
+        return this.texto;
+    }
+    public ImageIcon getImage(){
+        return this.imagen;
+    }
+    public Grupo getGrupo(){
+        return this.grupo;
+    }
+    public int getDescargas(){
+        return this.nDescargas;
+    }
+
+    public void setIdLectura(int num){
+        this.idLectura = num;
+    }
+    public void setEstado(String status){
+        this.estado = status;
+    }
+    public void setTexto(String txt){
+        this.texto = txt;
+    }
+    public void getImage(ImageIcon img){
+        this.imagen = img;
+    }
+    public void setGrupo(Grupo group){
+        this.grupo = group;
+    }
+    public void setDescargas(int desc){
+        this.nDescargas = desc;
+    }
+
+}
--- Titular.java
+++ Titular.java
@@ -1,10 +1,19 @@
+import java.util.List;
+
 public class Titular {
     private int id;
     private String nombre, tipo;
-    private List<Grupo> listaGrupo;
+    private List<Grupo> lGrupos;
     private List<Contenidos> listaContenidos;
     private float avanceTitular;
     private Institucion institucion;
+
+    public Titular(){
+        id = 0;
+        nombre ="";
+        tipo = "";
+
+    }
 
     public Titular(String datos){
         String[] sp = datos.split("_");
