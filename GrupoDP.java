7b714f8bba5542b4f4f42ff28994a94d22e930a6
Gustavo Hernández <GusHS@MacBook-Air-de-Gustavo.local>
2018-11-21 15:09:53 -0600

    added grade attribute to group class

--- GrupoDP.java
+++ GrupoDP.java
@@ -4,15 +4,18 @@ import java.util.List;
 public class GrupoDP {
     private List<AlumnoDP> lAlumnos;
     private float avanceGeneral;
+    private int grado;
 
     public GrupoDP(){
         this.lAlumnos = new LinkedList<AlumnoDP>();
         this.avanceGeneral = 0;
+        this.grado = 0;
     }
 
-    public GrupoDP(List<AlumnoDP> lAlumn, float av){
+    public GrupoDP(List<AlumnoDP> lAlumn, float av, int grade){
         this.lAlumnos = lAlumn;
         this.avanceGeneral = av;
+        this.grado = grade;
     }
 
     public List<AlumnoDP> getListaAlumnos(){
@@ -21,11 +24,17 @@ public class GrupoDP {
     public float getAvanceGeneral(){
         return this.avanceGeneral;
     }
+    public int getGrado(){
+        return this.grado;
+    }
 
     public void setListaAlumnos(List<AlumnoDP> lAlumn){
         this.lAlumnos = lAlumn;
     }
     public void setAvanceGeneral(float avance){
         this.avanceGeneral = avance;
+    }
+    public void setGrado(int grade){
+        this.grado = grade;
     }
 }
